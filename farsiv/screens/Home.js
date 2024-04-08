52b6b0dce0a12438180e47f5c6da28f60e91aba7
Mustafa KARATAŞ <91211621+MUSTAFAKARATAS0@users.noreply.github.com>
2024-04-08 17:13:00 +0300

    logout işlemleri

--- farsiv/screens/Home.js
+++ farsiv/screens/Home.js
@@ -8,7 +8,6 @@ const Home = () => {
 const [state] =useContext(AuthContext)
     return (
     <View style={styles.container}>
-      <Text>Home</Text>
       <Text>{JSON.stringify(state,null,4)}</Text>
       <FooterMenu/>
     </View>
@@ -19,7 +18,7 @@ const styles =StyleSheet.create({
     container:{
         flex:1,
         margin:10,
-
+        marginTop:40,
         justifyContent:"space-between"
     }
 })
